f44cdf0b81d284ebf015e2ecc052696402c6d9f3
Robert <robertkng104@gmail.com>
2019-10-27 17:06:07 +0000

    Better images, testing using SatPy

--- src/main.py
+++ src/main.py
@@ -1,22 +1,20 @@
 import netCDF4 as nt
 import matplotlib.pyplot as plt
 import numpy as np
+from satpy import Scene, find_files_and_readers
+from datetime import datetime
 
-from mpl_toolkits.basemap import Basemap
 
-
-def get_nc_files(year, month, day, ext=".nc"):
+def get_nc_files(year, month, day, time, ext=".nc"):
     from glob import glob
     import os
     str = os.path.join("..", "data", f"NPPSoumi {year}-{month}-{day}", f"*{ext}")
     return glob(str)
 
 
-def load_file(file, band="I05"):
+def load_file(img_file, geo_file=None, band="I05"):
     """Read in NETCDF4 file - return scaled map"""
-    with nt.Dataset(file, "r+", format="NETCDF5") as rootgrp:
-        lat_anchors = rootgrp.GRingPointLatitude
-        long_anchors = rootgrp.GRingPointLongitude
+    with nt.Dataset(img_file, "r+", format="NETCDF5") as rootgrp:
         try:
             obs_data_band = rootgrp.groups["observation_data"].variables[band][:]
             obs_lookup_band = rootgrp.groups["observation_data"].variables[band + "_brightness_temperature_lut"][:]
@@ -26,17 +24,20 @@ def load_file(file, band="I05"):
                 obs_data_band /= rootgrp.groups["observation_data"].variables[band].scale_factor
             except AttributeError as e:
                 raise e
-            # lat = np.zeros(obs_data_band.shape)
-            # long = np.zeros(obs_data_band.shape)
-            #
-            # lat[]
-            # xv, yv = np.meshgrid(np.arange(0, obs_data_band.shape[0]), np.arange(0, obs_data_band.shape[1]))
-            # long = long_anchors[0] + xv * (long_anchors[1] - long_anchors[0]) / ob + yv * (
-            #             long_anchors[3] - long_anchors[0])
-            # long =
-            return obs_lookup_band[obs_data_band.astype("int")]
         except KeyError as e:
             raise e
+    if geo_file is not None:
+        with nt.Dataset(geo_file, "r+", format="NETCDF5") as rootgrp:
+            try:
+                latitude = rootgrp.groups["geolocation_data"].variables["latitude"]
+                longitude = rootgrp.groups["geolocation_data"].variables["longitude"]
+
+                return obs_lookup_band[obs_data_band.astype("int")], latitude, longitude
+            except KeyError as e:
+                print("Are you sure this is a geolocation file")
+                raise e
+
+    return obs_lookup_band[obs_data_band.astype("int")]
 
 
 def select_and_plot(temps, eye_x, eye_y, padding=50):
@@ -47,17 +48,6 @@ def select_and_plot(temps, eye_x, eye_y, padding=50):
     plt.show()
 
 
-def plot_using_bmap(temperatures, lat, longs):
-    bmap = Basemap(width=100000, height=100000, resolution="l", projection="stere", lat_0=lat.mean(),
-                   lon_0=longs.mean())
-    la, lo = np.meshgrid(lat, longs)
-    x_i, y_i = bmap(la, lo)
-
-    bmap.pcolor(x_i, y_i, temperatures)
-    bmap.drawcountries()
-    plt.show()
-
-
 def plot_using_imshow(temps):
     fig, ax = plt.subplots()
     im = ax.imshow(temps, cmap="jet")
@@ -65,16 +55,24 @@ def plot_using_imshow(temps):
     plt.show()
 
 
-def rect_sample_profile(temps, eye_x, eye_y,width=5, max_r=150 ):
-    eye = temps[eye_x:eye_x-max_r:-1, eye_y - width:eye_y + width]
+def rect_sample_profile(temps, eye_x, eye_y, width=5, max_r=150):
+    eye = temps[eye_x:eye_x - max_r:-1, eye_y - width:eye_y + width]
     r = np.arange(0, max_r)
     t = np.mean(eye, axis=1)
-    plt.plot(r,t)
+    plt.plot(r, t)
     plt.show()
 
-files = get_nc_files(2017, 9, 19)
-temps_i05 = load_file("../data/NPPSoumi 2017-9-19/VNP02IMG.A2017262.1742.001.2017335035656.nc")
-temps_i04 = load_file("../data/NPPSoumi 2017-9-19/VNP02IMG.A2017262.1742.001.2017335035656.nc", band="I04")
-t = temps_i05 - temps_i04
-select_and_plot(t, 330, 2360)
-rect_sample_profile(t,330,2360,max_r = 75,width=10)
+
+filenames = find_files_and_readers(base_dir="../data", reader="viirs_l1b", start_time=datetime(2017, 9, 19),
+                                   end_time=datetime(2017, 9, 20))
+
+print(filenames)
+scene = Scene(reader="viirs_l1b",filenames=filenames)
+scene.load(["I04","I05"])
+scene.show("I04")
+# files = get_nc_files(2017, 9, 19)
+# temps_i05 = load_file("../data/NPPSoumi 2017-9-19/VNP03IMG.A2017262.1600.001.2017335033838.nc")
+# temps_i04 = load_file("../data/NPPSoumi 2017-9-19/VNP02IMG.A2017262.1742.001.2017335035656.nc", band="I04")
+# t = temps_i05 - temps_i04
+# select_and_plot(t, 330, 2360)
+# rect_sample_profile(t, 330, 2360, max_r=75, width=10)
