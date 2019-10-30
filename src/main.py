991b085b9c1a4cddd35bc44672067df6b063dae9
KloskaT <tpk116@ic.ac.uk>
2019-10-30 14:18:48 +0000

    can now look at other wavelengths, look at geo data, plot i05vsi04

--- src/main.py
+++ src/main.py
@@ -1,15 +1,14 @@
 import netCDF4 as nt
-import matplotlib.pyplot as plt
-import numpy as np
-import os
-os.environ['PROJ_LIB'] = 'C:\\Users\\tpklo\\.conda\\pkgs\\proj4-5.2.0-ha925a31_1\\Library\\share'
-
 from satpy import Scene, find_files_and_readers
 from datetime import datetime
+import matplotlib.pyplot as plt
+import numpy as np
+import h5py
+#import os
+#os.environ['PROJ_LIB'] = 'C:\\Users\\tpklo\\.conda\\pkgs\\proj4-5.2.0-ha925a31_1\\Library\\share'
 
-from mpl_toolkits.basemap import Basemap
 
-def get_nc_files(year, month, day, time, ext=".nc"):
+def get_nc_files(year, month, day, ext=".nc"):
     from glob import glob
     import os
     str = os.path.join("..", "data", f"NPPSoumi {year}-{month}-{day}", f"*{ext}")
@@ -19,39 +18,44 @@ def get_nc_files(year, month, day, time, ext=".nc"):
 def load_file(img_file, geo_file=None, band="I05"):
     """Read in NETCDF4 file - return scaled map"""
     with nt.Dataset(img_file, "r+", format="NETCDF5") as rootgrp:
-        try:
-            obs_data_band = rootgrp.groups["observation_data"].variables[band][:]
-            obs_lookup_band = rootgrp.groups["observation_data"].variables[band + "_brightness_temperature_lut"][:]
-            obs_data_band[obs_data_band == rootgrp.groups["observation_data"].variables[band]._FillValue] = np.nan
+        if band == "I04" or band == "I05":
             try:
-                obs_data_band += rootgrp.groups["observation_data"].variables[band].add_offset
-                obs_data_band /= rootgrp.groups["observation_data"].variables[band].scale_factor
-            except AttributeError as e:
+                obs_data_band = rootgrp.groups["observation_data"].variables[band][:]
+                obs_lookup_band = rootgrp.groups["observation_data"].variables[band + "_brightness_temperature_lut"][:]
+                obs_data_band[obs_data_band == rootgrp.groups["observation_data"].variables[band]._FillValue] = np.nan
+                try:
+                    obs_data_band += rootgrp.groups["observation_data"].variables[band].add_offset
+                    obs_data_band /= rootgrp.groups["observation_data"].variables[band].scale_factor
+                except AttributeError as e:
+                    raise e
+            except KeyError as e:
                 raise e
-        except KeyError as e:
-            raise e
+        else:
+            obs_data_band = rootgrp.groups["observation_data"].variables[band][:]
+            obs_data_band += rootgrp.groups["observation_data"].variables[band].radiance_add_offset
+            obs_uncert = rootgrp.groups["observation_data"].variables[band + "_uncert_index"][:]
+            obs_data_band /= (1 + obs_uncert * rootgrp.groups["observation_data"].variables[band].radiance_scale_factor**2)
+            return obs_data_band
     if geo_file is not None:
         with nt.Dataset(geo_file, "r+", format="NETCDF5") as rootgrp:
             try:
-                latitude = rootgrp.groups["geolocation_data"].variables["latitude"]
-                longitude = rootgrp.groups["geolocation_data"].variables["longitude"]
-
-                return obs_lookup_band[obs_data_band.astype("int")], latitude, longitude
+                latitude = rootgrp.groups["geolocation_data"].variables["latitude"][:]
+                longitude = rootgrp.groups["geolocation_data"].variables["longitude"][:]
+                return obs_lookup_band[obs_data_band.astype("int")], longitude, latitude
             except KeyError as e:
                 print("Are you sure this is a geolocation file")
                 raise e
-
     return obs_lookup_band[obs_data_band.astype("int")]
 
 
-def select_and_plot(temps, eye_x, eye_y, padding=50):
+def plot_eye(temps, eye_x, eye_y, padding=50):
     eye = temps[eye_x - padding:eye_x + padding, eye_y - padding: eye_y + padding]
     fig, ax = plt.subplots()
     im = ax.imshow(eye, cmap="jet")
     fig.colorbar(im)
 
 
-def plot_using_imshow(temps):
+def plot_whole_im(temps):
     fig, ax = plt.subplots()
     im = ax.imshow(temps, cmap="jet")
     fig.colorbar(im)
@@ -71,30 +75,35 @@ def rect_sample_profile(i05temps, i04temps, eye_x, eye_y,width=5, max_r=150, typ
         plt.xlabel('Radius from centre/m')
         plt.legend()
     elif type == 'compare':
-        plt.plot(i05t-i04t, i05t)
-        plt.xlabel('I05-I04')
+        plt.plot(i04t, i05t)
+        plt.xlabel('I04')
         plt.ylabel('I05')
 
 
+def combined_imaging_bands(filename, eye_coords=None):
+    bands = ["I01", "I02", "I03"]
+    for band in bands:
+        temps = load_file(
+            "C:/Users/tpklo/OneDrive/Documents/MSci/InitialCode/Data/VNP02IMG.A2017262.1742.001.2017335035656.nc",
+            band=band)
+        temps_arr = np.array(temps)
+        if band == "I01":
+            temps_combined = temps_arr
+        else:
+            temps_combined += temps_arr
+    if eye_coords is None:
+        plot_whole_im(temps_combined)
+    else:
+        plot_eye(temps_combined, eye_coords[0], eye_coords[1])
 
-filenames = find_files_and_readers(base_dir="../data", reader="viirs_l1b", start_time=datetime(2017, 9, 19),
-                                   end_time=datetime(2017, 9, 20))
+#filenames = find_files_and_readers(base_dir="../Data", reader="viirs_l1b", start_time=datetime(2017, 9, 19), end_time=datetime(2017, 9, 20))
+#scene = Scene(reader="viirs_l1b", filenames=filenames)
+#scene.load(["I04","I05"])
+#files = get_nc_files(2017, 9, 19)
 
-print(filenames)
-scene = Scene(reader="viirs_l1b",filenames=filenames)
-scene.load(["I04","I05"])
-scene.show("I04")
-# files = get_nc_files(2017, 9, 19)
-# temps_i05 = load_file("../data/NPPSoumi 2017-9-19/VNP03IMG.A2017262.1600.001.2017335033838.nc")
-# temps_i04 = load_file("../data/NPPSoumi 2017-9-19/VNP02IMG.A2017262.1742.001.2017335035656.nc", band="I04")
-# t = temps_i05 - temps_i04
-# select_and_plot(t, 330, 2360)
-# rect_sample_profile(t, 330, 2360, max_r=75, width=10)
-files = get_nc_files(2017, 9, 19)
-temps_i05 = load_file("C:/Users/tpklo/OneDrive/Documents/MSci/InitialCode/Data/VNP02IMG.A2017262.1742.001.2017335035656.nc")
+temps_i05, longs, lats = load_file("C:/Users/tpklo/OneDrive/Documents/MSci/InitialCode/Data/VNP02IMG.A2017262.1742.001.2017335035656.nc",
+                      geo_file="C:/Users/tpklo/OneDrive/Documents/MSci/InitialCode/Data/VNP03IMG.A2017262.1742.001.2017335033241.nc")
 temps_i04 = load_file("C:/Users/tpklo/OneDrive/Documents/MSci/InitialCode/Data/VNP02IMG.A2017262.1742.001.2017335035656.nc", band="I04")
-t = temps_i05 - temps_i04
-select_and_plot(t, 330, 2360)
-rect_sample_profile(temps_i05, temps_i04, 330, 2360, max_r=75, width=10, type='both')
+rect_sample_profile(temps_i05, temps_i04, 330, 2360, max_r=75, width=5, type='compare')
 
 plt.show()
