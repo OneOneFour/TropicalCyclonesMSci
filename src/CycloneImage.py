7f541c78891334c78259d3cc50d420f3f13bb8a2
Robert King <robertkng104@gmail.com>
2019-11-05 20:27:48 +0000

    Proper smart plotting - TODO integrate with Toms CSV stuff to fully automate plotting

--- src/CycloneImage.py
+++ src/CycloneImage.py
@@ -1,6 +1,8 @@
 from datetime import datetime
 
 import matplotlib.pyplot as plt
+from dask.diagnostics import ProgressBar
+from pyresample import create_area_def
 from satpy import Scene
 
 from fetch_file import get_data
@@ -33,27 +35,47 @@ class CycloneImage:
         self.raw_scene = Scene(filenames=self.files, reader="viirs_l1b")  # Raw Scene object storing the raw data
         self.raw_scene.load(["I04", "I05", "i_lat", "i_lon"])  # Load the two primary bands of inspection + geodata
 
-    def plot_globe(self, band="I04", proj="lcc"):
+        area = create_area_def("eye_area",
+                               {"proj": "lcc", "ellps": "WGS84", "lat_0": self.center[0], "lon_0": self.center[1],
+                                "lat_1": self.center[0]},
+                               width=200, height=200, units="degrees",
+                               area_extent=[self.center[1] - self.margin[1], self.center[0] - self.margin[0],
+                                            self.center[1] + self.margin[1], self.center[0] + self.margin[0]]
+                               )
+        self.core_scene = self.raw_scene.resample(area)
+
+    def plot_globe(self, band="I04", proj="lcc", sf=1.0):
         try:
             crs = self.corrected_scene[band].attrs["area"].to_cartopy_crs()
         except AttributeError:
             area = self.raw_scene[band].attrs["area"].compute_optimal_bb_area(
-                {"proj": proj, "lat_0": self.center[0], "lon_0": self.center[1], "lat_1": 25., "lat_2": 25.}
+                {"proj": proj, "lat_0": self.center[0], "lon_0": self.center[1], "lat_1": 10, "lat_2": 20}
             )
             self.corrected_scene = self.raw_scene.resample(area)  # resample image to projection
             crs = self.corrected_scene[band].attrs["area"].to_cartopy_crs()
-
-        # Cartopy methods
         ax = plt.axes(projection=crs)
+        # Cartopy methods
         ax.coastlines()
         ax.gridlines()
         ax.set_global()
-
-        plt.imshow(self.corrected_scene[band], transform=crs, extent=crs.bounds, origin="upper")
+        plt.imshow(self.corrected_scene[band], transform=crs,
+                   extent=(
+                       crs.bounds[0] * sf, crs.bounds[1] * sf, crs.bounds[2] * sf,
+                       crs.bounds[3] * sf),
+                   origin="upper")
         cb = plt.colorbar()
         cb.set_label("Kelvin (K)")
         plt.show()
 
+    def draw_rect(self, center, w, h):
+        splice = self.core_scene.crop(xy_bbox=[center[0] - w/2,center[1] - h/2,center[0] + w/2,center[1] + h/2])
+
+        plt.scatter(splice["I04"].data.flatten().compute(), splice["I05"].data.flatten().compute())
+        plt.show()
+
 
 if __name__ == "__main__":
-    pass
+
+    with ProgressBar():
+        ci = CycloneImage(2017, 9, 19, center=(16.58, -63.52), margin=(0.5, 0.5))
+        ci.draw_rect((-20000, 0), 5000, 40000)
