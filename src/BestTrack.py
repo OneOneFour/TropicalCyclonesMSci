17e69b956bdab7fc3e5826fc0c97535d380697ab
Robert King <robertkng104@gmail.com>
2019-11-18 20:00:54 +0000

    added some numpy methods and made them the default for new files plus added units to inner box

--- src/BestTrack.py
+++ src/BestTrack.py
@@ -1,16 +1,16 @@
 import os
 import pandas as pd
-from CycloneImage import get_eye
+from CycloneImage import get_eye,wrap
+from dask.diagnostics.progress import ProgressBar
 
 BEST_TRACK_CSV = os.environ.get("BEST_TRACK_CSV", "data/best_fit_csv/ibtracs.last3years.list.v04r00.csv")
-
 best_track_df = pd.read_csv(BEST_TRACK_CSV, skiprows=[1], na_values=" ", keep_default_na=False,
                             usecols=["SID", "ISO_TIME", "USA_SSHS", "LAT", "LON", "USA_STATUS", "USA_WIND", "USA_PRES",
                                      "USA_RMW", "BASIN", "NAME"])
 best_track_df["ISO_TIME"] = pd.to_datetime(best_track_df["ISO_TIME"])
 cat_4_5_all_basins = best_track_df.loc[
-    (best_track_df["USA_SSHS"] > 3) & (best_track_df["ISO_TIME"] > pd.Timestamp(year=2017, month=9, day=17))]
-
+    (best_track_df["USA_SSHS"] > 3) & (best_track_df["ISO_TIME"] > pd.Timestamp(year=2013, month=10, day=18))]
+cat_4_5_all_basins["LON"] = cat_4_5_all_basins["LON"].map(wrap)
 cat_4_5_all_basins_group = cat_4_5_all_basins.groupby(["SID"])
 print(len(cat_4_5_all_basins_group))
 for name, cyclone in cat_4_5_all_basins_group:
@@ -19,9 +19,10 @@ for name, cyclone in cat_4_5_all_basins_group:
         start_point = cyclone_point
         end_point = dict_cy[i + 1]
         ci = get_eye(start_point, end_point, name=start_point["NAME"], basin=start_point["BASIN"],
-                     cat=start_point["USA_SSHS"])
+                     cat=start_point["USA_SSHS"],dayOrNight="D")
         if ci is not None:
-            ci.draw_eye("I04")
-            ci.draw_eye("I05")
-            ci.save_object()
-            ci.draw_rect((0, 0), ci.rmw*4, ci.rmw*4)
+            with ProgressBar():
+                ci.draw_eye("I04")
+                ci.draw_eye("I05")
+                ci.save_object()
+                ci.draw_rect((0, 0), ci.rmw*1.5, ci.rmw*1.5)
--- src/fetch_file.py
+++ src/fetch_file.py
@@ -1,9 +1,9 @@
 import os
 import xml.etree.ElementTree as ET
-from datetime import datetime, timedelta
-import wget
+from datetime import datetime
+
 import requests
-import satpy
+import wget
 
 WEBSERVER_QUERY_URL = "http://modwebsrv.modaps.eosdis.nasa.gov/axis2/services/MODAPSservices"
 
@@ -11,11 +11,12 @@ SEARCH_FOR_FILES = "/searchForFiles"
 GET_FILE_URLS = "/getFileUrls"
 
 
-# TODO find way to apply this filter after the first initial query, aka by looking inside the ncdump metadata
-def get_data_single_date(root_dir, year, month, day, north=90, south=-90, west=-180, east=180, collection="5110", dayOrNight="DNB",
-             mode="download"):
+# DEPREC
+def get_data_single_date(root_dir, year, month, day, north=90, south=-90, west=-180, east=180, collection="5110",
+                         dayOrNight="DNB",
+                         mode="download"):
     '''
-    Use SatPy to check if data exists already in root dir. If not contact the NASA LAADS DAC server to download the required data.
+    FUNCTION DEPRECIATED - USE get_data() instead
     '''
 
     # TODO: Is there away to get the filepath without two requests server? Doesn't seem to be be studying the API documentation
@@ -59,10 +60,31 @@ def get_data_single_date(root_dir, year, month, day, north=90, south=-90, west=-
             if not os.path.isfile(file):
                 raise ConnectionError(
                     f"File {files[i]} could not be downloaded to {file}. Please check your internet connection and try again!")
+    return fpath, files
+
+
+def download_files_from_server(root_dir, file_urls):
+    fpath = [os.path.join(root_dir, os.path.split(f)[1]) for f in file_urls]
+    for i, file in enumerate(fpath):
+        if os.path.isfile(file):
+            print(f"{file} already downloaded... skipping...")
+            continue
+        print(f"Downloading {file}")
+        # Begin downloading from the content server
+        header = {
+            "Authorization": os.environ["LAADS_API_KEY"]
+        }
+        # Use wget for large files
+        # headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+        wget.download(file_urls[i], file)
+        if not os.path.isfile(file):
+            raise ConnectionError(
+                f"File {file_urls[i]} could not be downloaded to {file}. Please check your internet connection and try again!")
     return fpath
 
 
-def get_data(root_dir, start_time,end_time, north=90, south=-90, west=-180, east=180, collection="5110", dayOrNight="DNB",
+def get_data(root_dir, start_time, end_time, north=90, south=-90, west=-180, east=180, collection="5110",
+             dayOrNight="DNB",
              mode="download"):
     '''
     Use SatPy to check if data exists already in root dir. If not contact the NASA LAADS DAC server to download the required data.
@@ -91,21 +113,7 @@ def get_data(root_dir, start_time,end_time, north=90, south=-90, west=-180, east
             return None
         fileIdStr += child.text + ","
     file_name_response = requests.get(WEBSERVER_QUERY_URL + GET_FILE_URLS, params={"fileIds": fileIdStr})
+    if file_name_response.status_code != 200:
+        raise ConnectionError(file_name_response.content)
     files = [f.text for f in ET.fromstring(file_name_response.content)]
-    fpath = [os.path.join(root_dir, os.path.split(f)[1]) for f in files]
-    for i, file in enumerate(fpath):
-        if os.path.isfile(file):
-            print(f"{file} already downloaded... skipping...")
-            continue
-        print(f"Downloading {file}")
-        # Begin downloading from the content server
-        header = {
-            "Authorization": os.environ["LAADS_API_KEY"]
-        }
-        # Use wget for large files
-        # headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
-        wget.download(files[i], file)
-        if not os.path.isfile(file):
-            raise ConnectionError(
-                f"File {files[i]} could not be downloaded to {file}. Please check your internet connection and try again!")
-    return fpath
+    return download_files_from_server(root_dir, files), files
