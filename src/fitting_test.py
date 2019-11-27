0e659902bc8d92de63448a04f394fdd5612d6bd7
KloskaT <tpk116@ic.ac.uk>
2019-11-27 16:31:38 +0000

    Some estimates for error included

--- src/fitting_test.py
+++ src/fitting_test.py
@@ -25,7 +25,8 @@ def gt_min_i04(i04flat, i05flat):
     i05_fit_data = np.delete(i05flat, idxs[0])
     i04min_ind = np.unravel_index(np.argmin(i04_fit_data, axis=None), i04_fit_data.shape)
     gt = i05_fit_data[i04min_ind]
-    return gt
+    gt_err = 0.5
+    return gt, gt_err
 
 
 def gt_curve_fit(i04flat, i05flat, mode="min", plot=False):
@@ -35,6 +36,7 @@ def gt_curve_fit(i04flat, i05flat, mode="min", plot=False):
 
     minimised_i05_fit_data = np.arange(int(min(i05_fit_data)), int(max(i05_fit_data)), 1)
     minimised_i04_fit_data = []
+    num_vals_bins = []
 
     for i in minimised_i05_fit_data:
         min_idxs = np.where(np.logical_and(i05_fit_data>(i-0.5), i05_fit_data<(i+0.5)))[0]
@@ -49,19 +51,28 @@ def gt_curve_fit(i04flat, i05flat, mode="min", plot=False):
         else:
             minimised_i05_fit_data = np.delete(minimised_i05_fit_data, np.where(minimised_i05_fit_data == i)[0])
 
+        num_vals_bins.append(len(i04_min_vals))         # list of number of I04 values that were in each I05 increment (for errors)
+
     params, cov = sp.curve_fit(curve_func, minimised_i05_fit_data, minimised_i04_fit_data)
+    # perr = np.sqrt(np.diag(cov))
+
     xvalues = np.arange(min(minimised_i05_fit_data), max(minimised_i05_fit_data), 1)
     yvalues = curve_func(xvalues, params[0], params[1], params[2], params[3])
+    yvalues_worse = curve_func(xvalues, params[0], params[1], params[2], params[3])
     i04min_ind = np.argmin(yvalues)
     gt = xvalues[i04min_ind]
+    # satellite_data_error = ??
+    gt_err = ((0.5 * abs(3*params[0]*gt**2 + 2*params[1]*gt + params[2]))**2 + (num_vals_bins[i04min_ind] * 0.5 ** 2)) ** 0.5
 
     if plot:
         plt.scatter(i05flat, i04flat, s=0.25)
-        plt.plot(minimised_i05_fit_data, minimised_i04_fit_data)
-        plt.plot(xvalues, yvalues)
+        plt.plot(minimised_i05_fit_data, minimised_i04_fit_data, label="Data to be fitted")
+        plt.plot(xvalues, yvalues, label="Line of best fit")
+        plt.plot(xvalues, yvalues_worse, label="Line of worst fit")
+        plt.legend()
         plt.show()
 
-    return gt
+    return gt, gt_err
 
 
 def gt_two_line_fit(i04flat, i05flat, mode="min", plot=False):
@@ -109,6 +120,7 @@ def gt_two_line_fit(i04flat, i05flat, mode="min", plot=False):
         try:
             intersection_idx = np.where(lower_yvalues<upper_yvalues)[0][0]
             gt = xvalues[intersection_idx]
+            gt_err = 0
 
             if plot:
                 plt.scatter(i05flat, i04flat, s=0.25)
@@ -121,7 +133,7 @@ def gt_two_line_fit(i04flat, i05flat, mode="min", plot=False):
             print("No min value")
             gt = 0
 
-    return gt
+    return gt, gt_err
 
 
 if __name__ == "__main__":
@@ -134,12 +146,13 @@ if __name__ == "__main__":
         with open(file_name, "rb") as file:
             i04, i05 = pickle.load(file)
 
-        gt_min = gt_min_i04(i04.flatten(), i05.flatten())
-        gt_curve = gt_curve_fit(i04.flatten(), i05.flatten(), mode="min", plot=True)
-        gt_straight = gt_two_line_fit(i04.flatten(), i05.flatten(), mode="min", plot=False)
+        gt_min, gt_min_err = gt_min_i04(i04.flatten(), i05.flatten())
+        gt_curve, gt_curve_err = gt_curve_fit(i04.flatten(), i05.flatten(), mode="min", plot=True)
+        gt_straight, gt_straight_err = gt_two_line_fit(i04.flatten(), i05.flatten(), mode="min", plot=False)
         curve_min_diff = abs(gt_curve - gt_min) / gt_min * 100
         straight_min_diff = abs(gt_straight - gt_min) / gt_min * 100
         curve_straight_diff = abs(gt_curve - gt_straight) / gt_straight * 100
-        print("Curve gt: %f, Straight Line gt: %f, Min gt: %f" % (gt_curve, gt_straight, gt_min))
+        print("Curve gt: %f+-%f, Straight Line gt: %f+-%f, Min gt: %f+-%f"
+              % (gt_curve, gt_curve_err, gt_straight, gt_straight_err, gt_min, gt_min_err))
         print("Curve-Straight Diff = %f, Curve-Min diff = %f, Straight-Min diff = %f"
               % (curve_straight_diff, curve_min_diff, straight_min_diff))
