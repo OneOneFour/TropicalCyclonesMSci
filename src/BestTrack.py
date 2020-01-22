Got rid of some of Thomas (my) legacy stuff. Fixed loading for CycloneSnapshot.py, added function to mask one side of data, fixed multiple masking so that different masks can be added as much as necessary.

src/BestTrack.py
@@ -5,10 +5,12 @@ from typing import List
 import numpy as np
 import pandas as pd
 from dask.diagnostics.progress import ProgressBar
+import matplotlib.pyplot as plt
 
 from CycloneImage import get_eye, get_entire_cyclone, CycloneImage
+from CycloneSnapshot import CycloneSnapshot
 
-BEST_TRACK_CSV = os.environ.get("BEST_TRACK_CSV", "data/best_fit_csv/ibtracs.last3years.list.v04r00.csv")
+BEST_TRACK_CSV = os.environ.get("BEST_TRACK_CSV", "Data/ibtracs.last3years.list.v04r00.csv")
 best_track_df = pd.read_csv(BEST_TRACK_CSV, skiprows=[1], na_values=" ", keep_default_na=False)
 best_track_df["ISO_TIME"] = pd.to_datetime(best_track_df["ISO_TIME"])
 
@@ -51,11 +53,13 @@ def all_cyclone_eyes_since(year, month, day, cat_min=4):
                     snapshot.save("test.snap")
 
 
-def get_cyclone_eye_name_image(name, year):
+def get_cyclone_eye_name_image(name, year, max_len=np.inf):
     df_cyclone = best_track_df.loc[(best_track_df["NAME"] == name) & (best_track_df["ISO_TIME"].dt.year == year)]
     dict_cy = df_cyclone.to_dict(orient="records")
     snap_list = []
     for i, cyclone_point in enumerate(dict_cy[:-1]):
+        if len(snap_list) >= max_len:
+            return snap_list
         start_point = cyclone_point
         end_point = dict_cy[i + 1]
         with ProgressBar():
@@ -64,7 +68,10 @@ def get_cyclone_eye_name_image(name, year):
             # if ci is not None:
             #     ci.draw_eye()
             #     return ci
-            snap_list.append(get_eye(start_point, end_point))
+            eye = get_eye(start_point, end_point)
+            if eye:
+                eye.save("proc/pickle_data/%s%i" % (name, len(snap_list)))
+                snap_list.append(get_eye(start_point, end_point))
     return snap_list
 
 
@@ -86,12 +93,24 @@ def get_cyclone_by_name(name, year, max_len=np.inf) -> List[CycloneImage]:
             #     return ci
             cy = get_entire_cyclone(start_point, end_point)
             if cy:
+                eye = cy.draw_eye()
+                eye.save("proc/pickle_data/%s%i" % (name, len(snap_list)))
                 snap_list.append(cy)
     return snap_list
 
 
 if __name__ == "__main__":
-    cis = get_cyclone_by_name("IRMA", 2017, max_len=1)
+    # cis = get_cyclone_by_name("IRMA", 2017, max_len=1)
 
-    #r = cis[0].draw_rectangle((16.5, -55.283), 250000, 250000)
-    r_2 = cis[0].draw_rectangle(((16.13, -61.9)), 100000, 250000)
+    c = CycloneSnapshot.load("proc/pickle_data/IRMA0")
+
+    c.mask_thin_cirrus()
+    c.mask_array_I05()
+    c.mask_half("left")
+    c.mask_half("bottom")
+    c.gt_fit()
+
+    plt.show()
+
+    # r = cis[0].draw_rectangle((16.5, -55.283), 250000, 250000)
+    # r_2 = cis[0].draw_rectangle(((16.13, -61.9)), 100000, 250000)

src/CycloneImage.py
@@ -8,7 +8,7 @@ from shapely import geometry
 from CycloneSnapshot import CycloneSnapshot
 from fetch_file import get_data
 
-DATA_DIRECTORY = os.environ.get("DATA_DIRECTORY", "../data")
+DATA_DIRECTORY = os.environ.get("DATA_DIRECTORY", "C:/Users/tpklo/Documents/MSciNonCloud/Data")
 DEFAULT_MARGIN = 0.
 RESOLUTION_DEF = (3.71 / 6371) * 2 * np.pi
 NM_TO_M = 1852

src/CycloneSnapshot.py
@@ -17,7 +17,7 @@ class CycloneSnapshot:
 
     @staticmethod
     def load(fpath):
-        with open(fpath) as file:
+        with open(fpath, "rb") as file:
             cs = pickle.load(file)
         return cs
 
@@ -91,7 +91,7 @@ class CycloneSnapshot:
                                self.pixel_x * 0.5 * self.shape[1] / 1000,
                                -self.pixel_y * 0.5 * self.shape[0] / 1000,
                                self.pixel_y * 0.5 * self.shape[0] / 1000])
-
+        ax.set_title("%s %s" % (self.meta_data["NAME"], self.meta_data["ISO_TIME"]))
         cb = plt.colorbar(im)
         if band == "M09":
             cb.set_label("Reflectance (%)")
@@ -120,10 +120,13 @@ class CycloneSnapshot:
         if self.M09 is None:
             raise ValueError("No M9 data present")
         if hasattr(self, "I04_mask") or hasattr(self, "I05_mask"):
-            self.I04_mask = npma.mask_or(self.I04_mask, npma.array(self.I04, mask=self.M09 >= reflectance_cutoff))
-            self.I05_mask = npma.mask_or(self.I05_mask, npma.array(self.I05, mask=self.M09 >= reflectance_cutoff))
-        self.I04_mask = npma.array(self.I04, mask=self.M09 >= reflectance_cutoff)
-        self.I05_mask = npma.array(self.I05, mask=self.M09 >= reflectance_cutoff)
+            new_I04_mask = npma.mask_or(self.I04_mask.mask, npma.array(self.I04, mask=self.M09 >= reflectance_cutoff).mask)
+            new_I05_mask = npma.mask_or(self.I05_mask.mask, npma.array(self.I05, mask=self.M09 >= reflectance_cutoff).mask)
+            self.I04_mask = npma.array(self.__I04, mask=new_I04_mask)
+            self.I05_mask = npma.array(self.__I05, mask=new_I05_mask)
+        else:
+            self.I04_mask = npma.array(self.I04, mask=self.M09 >= reflectance_cutoff)
+            self.I05_mask = npma.array(self.I05, mask=self.M09 >= reflectance_cutoff)
 
     def __flat(self, a):
         if isinstance(a, npma.MaskedArray):
@@ -164,19 +167,49 @@ class CycloneSnapshot:
         self.img_plot(fig, ax, band)
         plt.show()
 
-    def mask_array_I04(self, HIGH=273, LOW=210):
+    def mask_array_I04(self, HIGH=273, LOW=220):
         if hasattr(self, "I04_mask") or hasattr(self, "I05_mask"):
-            self.I04_mask = npma.mask_or(self.I04_mask, npma.masked_outside(self.__I04, LOW, HIGH))
+            new_I04_mask = npma.mask_or(self.I04_mask.mask, npma.masked_outside(self.__I04, LOW, HIGH).mask)
+            new_I05_mask = npma.mask_or(self.I05_mask.mask, npma.masked_outside(self.__I04, LOW, HIGH).mask)
+            self.I05_mask = npma.array(self.__I05, mask=new_I05_mask)
+            self.I04_mask = npma.array(self.__I04, mask=new_I04_mask)
         else:
             self.I04_mask = npma.masked_outside(self.__I04, LOW, HIGH)
-        self.I05_mask = npma.array(self.__I05, mask=self.I04_mask.mask)
+            self.I05_mask = npma.array(self.__I05, mask=self.I04_mask.mask)
 
-    def mask_array_I05(self, HIGH=273, LOW=210):
+    def mask_array_I05(self, HIGH=273, LOW=220):
         if hasattr(self, "I04_mask") or hasattr(self, "I05_mask"):
-            self.I05_mask = npma.mask_or(self.I05_mask, npma.masked_outside(self.__I05, LOW, HIGH))
+            new_I05_mask = npma.mask_or(self.I05_mask.mask, npma.masked_outside(self.__I05, LOW, HIGH).mask)
+            new_I04_mask = npma.mask_or(self.I04_mask.mask, npma.masked_outside(self.__I05, LOW, HIGH).mask)
+            self.I05_mask = npma.array(self.__I05, mask=new_I05_mask)
+            self.I04_mask = npma.array(self.__I04, mask=new_I04_mask)
         else:
             self.I05_mask = npma.masked_outside(self.__I05, LOW, HIGH)
-        self.I04_mask = npma.array(self.__I04, mask=self.I04_mask.mask)
+            self.I04_mask = npma.array(self.__I04, mask=self.I05_mask.mask)
+
+    def mask_half(self, half="right"):
+        blank_mask = np.zeros_like(self.__I05)
+        if half == "top":
+            idx = np.arange(0, int(len(self.__I05)/2))
+            blank_mask[idx, :] = 1
+        if half == "left":
+            idx = np.arange(0, int(len(self.__I05) / 2))
+            blank_mask[:, idx] = 1
+        if half == "bottom":
+            idx = np.arange(int(len(self.__I05) / 2), len(self.__I05))
+            blank_mask[idx, :] = 1
+        if half == "right":
+            idx = np.arange(int(len(self.__I05) / 2), len(self.__I05))
+            blank_mask[:, idx] = 1
+        blank_mask = blank_mask.astype("bool")
+        if hasattr(self, "I04_mask") or hasattr(self, "I05_mask"):
+            new_I05_mask = npma.mask_or(self.I05_mask.mask, blank_mask)
+            new_I04_mask = npma.mask_or(self.I04_mask.mask, blank_mask)
+            self.I05_mask = npma.array(self.__I05, mask=new_I05_mask)
+            self.I04_mask = npma.array(self.__I04, mask=new_I04_mask)
+        else:
+            self.I05_mask = npma.array(self.__I05, mask=blank_mask)
+            self.I04_mask = npma.array(self.__I04, mask=blank_mask)
 
     def gt_fit(self):
         gt_fitter = GTFit(self.__flat(self.I04), self.__flat(self.I05))
@@ -184,7 +217,7 @@ class CycloneSnapshot:
         fig, ax = plt.subplots(1, 2)
         self.img_plot(fig, ax[1])
         self.scatter_plot(fig, ax[0], gt_fitter)
-        plt.show()
+        #plt.show()
 
     def unmask_array(self):
         del self.I04_mask
@@ -195,113 +228,4 @@ class CycloneSnapshot:
             pickle.dump(self, file)
 
 
-    def show_fitted_pixels(self):
-        i05_flat = self.I05.flatten()
-        i04_flat = self.I04.flatten()
-        x_i05 = np.arange(220, 273, 1)
-        if len(x_i05) < 1:
-            return
-        y_i04 = np.array([0] * len(x_i05))
-        fig, axs = plt.subplots(1, 2)
-        im = axs[1].imshow(self.I05)
-        plt.colorbar(im)
-        axs[1].set_title("%s %s" % (self.name, self.core_scene.start_time.strftime('%Y-%m-%d')))
-        axs[0].scatter(i04_flat, i05_flat, s=0.25)
-        for i, x in enumerate(x_i05):
-            vals = i04_flat[np.where(np.logical_and(i05_flat > (x - 0.5), i05_flat < (x + 0.5)))]
-            vals_5_min = []
-            vals_5_min_i05val = []
-            if len(vals) < 1:
-                continue
-            percent_range = int(np.ceil(len(vals) * 0.075))
-            if x > 235:  # Takes minimum values lower than theoretical min gt and v.v.
-                for j in range(percent_range):
-                    if len(vals) == 0:  # Not all values of i05 will have 5 i04 values
-                        break
 
-                    vals_5_min.append(min(vals))
-                    i05s_with_same_i04 = i05_flat[np.where(i04_flat == min(vals))]
-                    for i05 in i05s_with_same_i04:
-                        if x - 0.5 < i05 < x + 0.5:
-                            vals_5_min_i05val.append(i05)
-                            if len(vals_5_min_i05val) > j:
-                                break
-
-                    vals = np.delete(vals, np.where(vals == min(vals)))
-                y_i04[i] = np.median(vals_5_min)
-                axs[0].scatter(vals_5_min, vals_5_min_i05val, color="orange", s=5)
-                # rect = self.rects[key]
-                # offset_x = (self.width - self.rects[key].width/self.pixel_x)/2
-                # offset_y = (self.height - self.rects[key].height/self.pixel_y)/2
-                for xy in range(len(vals_5_min)):
-                    points = np.argwhere(np.logical_and(self.I05 == vals_5_min_i05val[xy], self.I04 == vals_5_min[xy]))
-                    axs[1].scatter([p[1] for p in points], [p[0] for p in points], s=5, c="red")
-            else:
-                increasing_range = int(np.ceil(len(vals) * (0.075 + (235 - x) * 0.025)))
-                for j in range(percent_range):
-                    if len(vals) == 0:  # Not all values of i05 will have 5 i04 values
-                        break
-                    vals.sort()
-                    idx = int((235 - x) * 0.025 * len(vals) + j)  # changes idx to shift 2.5% range every x value
-                    vals_5_min.append(vals[idx])
-                    i05s_with_same_i04 = i05_flat[np.where(i04_flat == vals[idx])]
-                    for i05 in i05s_with_same_i04:
-                        if x - 0.5 < i05 < x + 0.5:
-                            vals_5_min_i05val.append(i05)
-                            if len(vals_5_min_i05val) > j:
-                                break
-
-                y_i04[i] = np.median(vals_5_min)
-                axs[0].scatter(vals_5_min, vals_5_min_i05val, color="black", s=5)
-                # rect = self.rects[key]
-                # offset_x = (self.width - self.rects[key].width / self.pixel_x) / 2
-                # offset_y = (self.height - self.rects[key].height / self.pixel_y) / 2
-                for xy in range(len(vals_5_min)):
-                    points = np.argwhere(np.logical_and(self.I05 == vals_5_min_i05val[xy], self.I04 == vals_5_min[xy]))
-                    axs[1].scatter([p[1] for p in points], [p[0] for p in points], s=5, c="black")
-
-        zero_args = np.where(y_i04 == 0)
-        x_i05 = np.delete(x_i05, zero_args)
-        y_i04 = np.delete(y_i04, zero_args)
-
-        params, cov = sp.curve_fit(cubic, x_i05, y_i04, absolute_sigma=True)
-
-        xvalues = np.arange(min(x_i05), max(x_i05), 1)
-        yvalues = cubic(xvalues, *params)
-
-        gt_ve = (-params[1] + np.sqrt(params[1] ** 2 - 3 * params[0] * params[2])) / (3 * params[0])
-        if np.iscomplex(gt_ve) or min(x_i05) > gt_ve > max(x_i05):
-            return
-        self.gt = [gt_ve]
-
-        axs[0].plot(yvalues, xvalues, color="r")
-        axs[0].invert_xaxis()
-        axs[0].invert_yaxis()
-        if 300 > gt_ve > 200:
-            axs[0].axhline(gt_ve, color="r")
-        plt.show()
-        return gt_ve
-
-    def half_eye(self):
-        I05_flat = self.I05.flatten()
-        I04_flat = self.I04.flatten()
-        rows = len(self.I05)
-        columns = len(self.I05[0])
-        half_mask = np.zeros(np.shape(self.I05))
-        for i in range(int(rows / 2)):
-            for j in range(int(columns)):
-                half_mask[i][j] = 1
-        masked_I05 = np.ma.masked_outside(self.I05, 220, 270)
-        # masked_I05 = np.ma.masked_array(masked_I05, mask=half_mask)
-        masked_I04 = self.I04
-        masked_rows = len(masked_I05)
-        masked_columns = len(masked_I05[0])
-        print(masked_rows, masked_columns)
-        fig, axs = plt.subplots(1, 2)
-        axs[0].scatter(self.I04, self.I05, s=0.25)
-        axs[0].scatter(masked_I04, masked_I05, s=0.5, c="black")
-        axs[1].imshow(self.I05)
-        axs[1].imshow(masked_I05, cmap="Dark2")
-        axs[0].invert_xaxis()
-        axs[0].invert_yaxis()
-        plt.show()