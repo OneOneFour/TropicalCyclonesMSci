6b56a8667430670ef6dd42ebecb32df505b092b5
Robert King <robertkng104@gmail.com>
2020-02-27 23:34:13 +0000

    tests working

--- src/tests/test_aerosol.py
+++ src/tests/test_aerosol.py
@@ -1,6 +1,5 @@
 from AerosolImage import *
 
-
 def test_MODIS_PATH():
     assert os.path.isdir(MODIS_PATH)
 
@@ -8,7 +7,7 @@ def test_MODIS_PATH_set():
     assert MODIS_PATH == os.getcwd()
 
 def test_aerosol_image():
-    checkpath = os.path.join(MODIS_PATH, str(2012), f"AerosolImage.001.gpz")
+    checkpath = os.path.join(MODIS_PATH, str(2012), f"AerosolImage.001.gzp")
     assert AerosolImageMODIS.path(2012, 1) == checkpath
 
 def test_aerosol_file():
